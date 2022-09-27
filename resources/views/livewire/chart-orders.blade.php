be45d21adf7b20423fe9ccc2221142ff6c6f4b51
chang180 <chang180@gmail.com>
2022-09-27 10:35:21 +0800

    ep02 completed

--- resources/views/livewire/chart-orders.blade.php
+++ resources/views/livewire/chart-orders.blade.php
@@ -12,11 +12,11 @@
             datasets: [{
                 label: `${this.selectedYear - 1} Orders`,
                 backgroundColor: 'lightgray',
-                data: {{ Js::from($lastYearOrders) }},
+                data: this.lastYearOrders,
             }, {
                 label: `${this.selectedYear} Orders`,
                 backgroundColor: 'lightgreen',
-                data: {{ Js::from($thisYearOrders) }},
+                data: this.thisYearOrders,
             }]
         };
 
@@ -30,10 +30,22 @@
             this.$refs.canvas,
             config
         );
+
+        Livewire.on('updateTheChart', () => {
+
+            myChart.data.datasets[0].data = this.lastYearOrders;
+            myChart.data.datasets[1].data = this.thisYearOrders;
+
+            myChart.data.datasets[0].label = `${this.selectedYear - 1} Orders`;
+            myChart.data.datasets[1].label = `${this.selectedYear} Orders`;
+
+            myChart.update();
+        });
+
     }
 }" class="mt-4">
     <span>Year: </span>
-    <select name="selectedYear" id="selectedYear" wire:model="selectedYear">
+    <select name="selectedYear" id="selectedYear" wire:model="selectedYear" wire:change="updateOrderCount">
         @foreach ($availableYears as $year)
             <option value="{{ $year }}">{{ $year }}</option>
         @endforeach
@@ -42,8 +54,14 @@
         Selected: <span x-text="selectedYear"></span>
     </div>
     <div class="my-6">
-        <div><span x-text="selectedYear - 1"></span> Orders: {{ array_sum($lastYearOrders) }}</div>
-        <div><span x-text="selectedYear"></span> Orders: {{ array_sum($thisYearOrders) }}</div>
+        <div>
+            <span x-text="selectedYear - 1"></span> Orders: 
+            <span x-text="lastYearOrders.reduce((a, b) => a + b, 0)"></span>
+        </div>
+        <div>
+            <span x-text="selectedYear"></span> Orders: 
+            <span x-text="thisYearOrders.reduce((a, b) => a + b, 0)"></span>
+        </div>
     </div>
     <canvas id="myChart" x-ref="canvas"></canvas>
 </div>
